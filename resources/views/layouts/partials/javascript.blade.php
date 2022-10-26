123270d4d02e9ab5282eb2f0db97ed847bddb55e
Israel <creativef25@gmail.com>
2022-10-26 17:33:54 -0500

    avances

---
+++ resources/views/layouts/partials/javascript.blade.php
@@ -0,0 +1,30 @@
+ <!-- Global Vendor, plugins JS -->
+
+    <!-- JS Files
+    ============================================ -->
+    <!-- Global Vendor, plugins JS -->
+
+    <!-- Vendor JS -->
+    <script src="{{ asset('motoconexion/assets/js/vendor/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ asset('motoconexion/assets/js/vendor/jquery-3.6.0.min.js')}}"></script>
+    <script src="{{ asset('motoconexion/assets/js/vendor/jquery-migrate-3.3.2.min.js')}}"></script>
+    <script src="{{ asset('motoconexion/assets/js/vendor/modernizr-3.11.2.min.js')}}"></script>
+
+    <!--Plugins JS-->
+    <script src="{{ asset('motoconexion/assets/js/plugins/wow.min.js')}}"></script>
+    <script src="{{ asset('motoconexion/assets/js/plugins/jquery-ui.min.js')}}"></script>
+    <script src="{{ asset('motoconexion/assets/js/plugins/swiper-bundle.min.js')}}"></script>
+    <script src="{{ asset('motoconexion/assets/js/plugins/jquery.nice-select.js')}}"></script>
+    <script src="{{ asset('motoconexion/assets/js/plugins/parallax.min.js')}}"></script>
+    <script src="{{ asset('motoconexion/assets/js/plugins/jquery.magnific-popup.min.js')}}"></script>
+    <script src="{{ asset('motoconexion/assets/js/plugins/tippy.min.js')}}"></script>
+    <script src="{{ asset('motoconexion/assets/js/plugins/ion.rangeSlider.min.js')}}"></script>
+    <script src="{{ asset('motoconexion/assets/js/plugins/mailchimp-ajax.js')}}"></script>
+
+    <!-- Minify Version -->
+    <!-- <script src="assets/js/vendor.min.js"></script> -->
+    <!-- <script src="assets/js/plugins.min.js"></script> -->
+
+    <!--Main JS (Common Activation Codes)-->
+    <script src="{{ asset('motoconexion/assets/js/main.js')}}"></script>
+    <!-- <script src="assets/js/main.min.js"></script> -->
